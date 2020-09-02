e5fa63646013ea535d1312c9f94a8f0185f05ad2
Peter <Posoo617@gmail.com>
2020-09-02 06:00:22 +0300

    change and update state functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import "./App.css"
 import Form from "./components/Form"
 import TodoList from "./components/TodoList"
 
 const App = () => {
+	const [inputText, setInputText] = useState("")
+	const [todos, setTodos] = useState([])
 	return (
 		<div>
 			<header>
-				<h1>Rd27's Todo List</h1>
+				<h1>Rd27's Todo List </h1>
 			</header>
-			<Form />
+			<Form
+				todos={todos}
+				setTodos={setTodos}
+				inputText={inputText}
+				setInputText={setInputText}
+			/>
 			<TodoList />
 		</div>
 	)
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 const TodoList = () => {
-	return <div>Todo List</div>
+	return (
+		<div className="todo-container">
+			<ul className="todo-list"></ul>
+		</div>
+	)
 }
 
 export default TodoList
